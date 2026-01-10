db821e859bbdf64423ec11b9314e9249cad79fb5
prakashtaz0091 <code.ing2468@gmail.com>
2026-01-10 23:06:46 +0545

    feat: jwt auth

---
+++ project/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns = [
+    path("default-admin/", admin.site.urls),
+    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
