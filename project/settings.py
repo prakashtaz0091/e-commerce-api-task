b82b516b0b1850384b2b12b2c87aa744e2cada27
prakashtaz0091 <code.ing2468@gmail.com>
2026-01-12 08:46:51 +0545

    feat: normal pagination to limit offset pagination

--- project/settings.py
+++ project/settings.py
@@ -51,8 +51,9 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
+    "MAX_LIMIT": 100,
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
     ],
